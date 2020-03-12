e35998135535b7458bd24fece0172e0dc0aabeea
TrevorZB <trevorzb11@gmail.com>
2020-03-12 18:52:59 -0500

    Updates

---
+++ precache-manifest.3be6f271f50e9d0362e68b1ea52abd5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fabb6a6bb97e28a274df6775d81791",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "bc07c2e1f35542701846",
+    "url": "/react-quote-generator/static/css/main.fa0fa649.chunk.css"
+  },
+  {
+    "revision": "63efc0f2a42bf8385dc1",
+    "url": "/react-quote-generator/static/js/2.5aed5a61.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-quote-generator/static/js/2.5aed5a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc07c2e1f35542701846",
+    "url": "/react-quote-generator/static/js/main.4027624e.chunk.js"
+  },
+  {
+    "revision": "86fcb5d2aa561f746461",
+    "url": "/react-quote-generator/static/js/runtime-main.56a8cca8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quote-app/precache-manifest.a501af39c8498c3b12801d1736d3e277.js"
+  "/react-quote-generator/precache-manifest.3be6f271f50e9d0362e68b1ea52abd5c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quote-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-quote-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
