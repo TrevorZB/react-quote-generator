cc39ea768cb57af940e8b573b280bf8000f3e601
TrevorZB <trevorzb11@gmail.com>
2020-03-12 19:11:11 -0500

    publishing to pages

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,6 @@ class App extends Component {
     }
     return (
       <div id="home">
-        <h1>HEREHELLO</h1>
         <Home
           handleInputChange={this.handleInputChange}
           input={this.state.input}
